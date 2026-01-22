9322897f34b2829d26a687b2be6a833cd946f05f
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-22 17:44:49 -0300

    Aula: Mapeando relacionamentos

--- src/domain/entities/Answer.ts
+++ src/domain/entities/Answer.ts
@@ -1,11 +1,21 @@
 import { randomUUID } from "node:crypto";
 
+type AnswerProps = {
+  content: string;
+  authorId: string;
+  questionId: string;
+}
+
 export class Answer {
   public id: string;
   public content: string;
+  public authorId: string;
+  public questionId: string;
 
-  constructor(content: string, id?: string) {
+  constructor({ content, authorId, questionId }: AnswerProps, id?: string) {
     this.id = id ?? randomUUID();
     this.content = content;
+    this.authorId = authorId;
+    this.questionId = questionId;
   }
 }
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -8,7 +8,11 @@ type AnswerQuestionUseCaseRequest = {
 
 export class AnswerQuestionUseCase {
   execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-    const answer = new Answer(content);
+    const answer = new Answer({
+      content,
+      authorId: instructorId,
+      questionId
+    });
 
     return answer;
   }
