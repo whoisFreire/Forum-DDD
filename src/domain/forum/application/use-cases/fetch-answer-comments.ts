15c3d2874bdcb91bbc6012289c27cb51a6ddd90a
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-25 01:50:27 -0300

    Aula: Caso de uso: Listar comentários da resposta

---
+++ src/domain/forum/application/use-cases/fetch-answer-comments.ts
@@ -0,0 +1,28 @@
+import { AnswerComment } from '../../enterprise/entities/answer-comment'
+import { AnswerCommentsRepository } from '../repositories/answer-comments-repository'
+
+type FetchAnswerCommentsUseCaseRequest = {
+  answerId: string
+  page: number
+}
+
+type FetchAnswerCommentsUseCaseResponse = {
+  answerComments: AnswerComment[]
+}
+
+export class FetchAnswerCommentsUseCase {
+  constructor(private readonly repository: AnswerCommentsRepository) {}
+
+  async execute({
+    answerId,
+    page,
+  }: FetchAnswerCommentsUseCaseRequest): Promise<FetchAnswerCommentsUseCaseResponse> {
+    const answerComments = await this.repository.findManyByAnswerId(answerId, {
+      page,
+    })
+
+    return {
+      answerComments,
+    }
+  }
+}
--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository'
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment'
 
@@ -14,6 +15,16 @@ export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepositor
       return null
     }
     return answerComment
+  }
+
+  async findManyByAnswerId(
+    answerId: string,
+    { page }: PaginationParams,
+  ): Promise<AnswerComment[]> {
+    const answerComments = this.items
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20)
+    return answerComments
   }
 
   async delete(answerComment: AnswerComment): Promise<void> {
