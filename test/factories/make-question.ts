5f2585bb65bde81137e8bbd1d7c4e2c58466c0a1
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-24 22:22:30 -0300

    Aula: Gerando dados fictícios

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -3,14 +3,21 @@ import {
   Question,
   QuestionProps,
 } from '@/domain/forum/enterprise/entities/question'
+import { faker } from '@faker-js/faker'
 
-export function makeQuestion(override: Partial<QuestionProps> = {}): Question {
-  const question = Question.create({
-    authorId: new UniqueEntityId('author-1'),
-    title: 'Sample Question',
-    content: 'This is a sample question content',
-    ...override,
-  })
+export function makeQuestion(
+  override: Partial<QuestionProps> = {},
+  id?: UniqueEntityId,
+): Question {
+  const question = Question.create(
+    {
+      authorId: new UniqueEntityId(),
+      title: faker.lorem.sentence(),
+      content: faker.lorem.text(),
+      ...override,
+    },
+    id,
+  )
 
   return question
 }
