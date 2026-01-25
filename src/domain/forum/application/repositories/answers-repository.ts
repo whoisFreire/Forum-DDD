32743840aafaa5c58952df345291f59c5d9ddfa5
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-24 21:30:34 -0300

    Aula: Refatorando as pastas

--- src/domain/forum/application/repositories/answers-repository.ts
+++ src/domain/forum/application/repositories/answers-repository.ts
@@ -1,4 +1,4 @@
-import { Answer } from '../entities/answer'
+import { Answer } from '../../enterprise/entities/answer'
 
 export interface AnswersRepository {
   Create(answer: Answer): Promise<void>
