8177d3f9a763eef3b7d062d7bfc618b619a60632
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-28 04:22:01 -0300

    Aula: Ouvindo um evento de domínio

---
+++ src/domain/notification/application/subscribers/on-answer-created.spec.ts
@@ -0,0 +1,24 @@
+import { makeAnswer } from 'test/factories/make-answer'
+import { OnAnswerCreated } from './on-answer-created'
+import { InMemoryAnswersRepository } from 'test/repositories/in-memory-answers-repository'
+import { InMemoryAnswerAttachmentsRepository } from 'test/repositories/in-memory-answer-attachments-repository'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+
+describe('On Answer Created', () => {
+  beforeEach(() => {
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
+    inMemoryAnswersRepository = new InMemoryAnswersRepository(
+      inMemoryAnswerAttachmentsRepository,
+    )
+  })
+
+  it('Should send a notification when an answer is created', async () => {
+    const onAnswerCreated = new OnAnswerCreated()
+
+    const answer = makeAnswer()
+    await inMemoryAnswersRepository.create(answer)
+  })
+})
