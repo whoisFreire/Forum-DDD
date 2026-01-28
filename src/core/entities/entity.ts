5415eac8d41371ece6bbe37001247a79aad841af
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-28 03:47:39 -0300

    Aula: Estrutura de eventos de domínio

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -12,4 +12,16 @@ export class Entity<T> {
   get id() {
     return this._id
   }
+
+  public equals(entity: Entity<unknown>) {
+    if (entity === this) {
+      return true
+    }
+
+    if (entity.id === this._id) {
+      return true
+    }
+
+    return false
+  }
 }
