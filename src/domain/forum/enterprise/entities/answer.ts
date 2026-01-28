c2ea575ee95aba16e9487ca1b65f450dd58d2789
Leonardo Freire Russi <freirerussi@gmail.com>
2026-01-28 04:06:02 -0300

    Aula: Evento: Resposta criada

--- src/domain/forum/enterprise/entities/answer.ts
+++ src/domain/forum/enterprise/entities/answer.ts
@@ -1,7 +1,8 @@
-import { Entity } from '@/core/entities/entity'
 import { UniqueEntityId } from '@/core/entities/value-objects/unique-entity-id'
 import { Optional } from '@/core/types/optional'
 import { AnswerAttachmentList } from './answer-attachment-list'
+import { AggregateRoot } from '@/core/entities/aggregate-root'
+import { AnswerCreatedEvent } from './events/answer-created-event'
 
 export type AnswerProps = {
   authorId: UniqueEntityId
@@ -12,7 +13,7 @@ export type AnswerProps = {
   updatedAt?: Date
 }
 
-export class Answer extends Entity<AnswerProps> {
+export class Answer extends AggregateRoot<AnswerProps> {
   get content() {
     return this.props.content
   }
@@ -67,6 +68,12 @@ export class Answer extends Entity<AnswerProps> {
       },
       id,
     )
+    const isNewAnswer = !id
+
+    if (isNewAnswer) {
+      answer.addDomainEvent(new AnswerCreatedEvent(answer))
+    }
+
     return answer
   }
 }
